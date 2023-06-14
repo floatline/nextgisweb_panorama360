eb54327b852b1ca59c996943b3d2242a5de184b8
floatline <poltewski@gmail.com>
2023-06-14 10:17:42 +0000

    Fix settings not saving

--- nextgisweb_panorama360/nodepkg/panorama360-settings/Panorama360Settings.js
+++ nextgisweb_panorama360/nodepkg/panorama360-settings/Panorama360Settings.js
@@ -1,22 +1,13 @@
 import { FieldsForm, useForm} from "@nextgisweb/gui/fields-form";
 import { useEffect, useState, useMemo } from "react";
 import i18n from "@nextgisweb/pyramid/i18n!gui";
-//{panorama_enabled, panorama_layer_field}
 export const Panorama360Settings = ( {onChange, values} ) =>  {
     
     const form = useForm()[0];
-    //const [values_, setValues_] = useState(values); - not needed
-    // const [panoramaEnabled, setPanoramaEnabled] = useState(panorama_enabled);
-    // const [panoramaLayerField, setPanoramaLayerField] = useState(panorama_layer_field);
-    // // const [values, setValues] = useState();
-
-
-
-    
 
     const fields =  [
         {
-            name: "panorama_enabled",
+            name: "enabled",
             label: i18n.gettext("Enable Panorama360"),
             widget: "checkbox",
             required: true,
@@ -26,7 +17,6 @@ export const Panorama360Settings = ( {onChange, values} ) =>  {
             name: "panorama_layer_field",
             label: i18n.gettext("Field name"),
             required: true,
-            //disabled: !panorama_enabled,
             widget: "input"
         }
     ] 
@@ -34,29 +24,9 @@ export const Panorama360Settings = ( {onChange, values} ) =>  {
 
 
     useEffect(() => { 
-        //update form
         form.setFieldsValue(values);
-        console.log("I'm in useEffect")
     }, [values]) 
 
-    
-    
-    // const updateField()
-
-    // const onEnabledChange = (val) => {
-    //     setPanoramaEnabled(val);
-    // };
-
-    // const onFieldsChange = (val) => {
-    //     setPanoramaLayerField(val);
-    // }
-
-    const onFieldsChange = (changedFields) => {
-       console.log(...changedFields);
-       console.log("I'm running onFieldsChange")
-    };
-
-    //const initVals = [true, null];
     return (
            <div className="ngw-panorama360-settings-form"
            style={{padding: "16px"}}>
@@ -64,11 +34,12 @@ export const Panorama360Settings = ( {onChange, values} ) =>  {
             <FieldsForm 
                 fields={fields}
                 form={form}
-                onFieldsChange={onFieldsChange}
                 initialValues={values}
-                // onChange={ 
-                //     ({ value })
-                // }
+                onChange={async (v) => {
+                    if (await v.isValid()) {
+                        onChange(v.value);
+                    }
+                }}
             >
             </FieldsForm>
             </div>
