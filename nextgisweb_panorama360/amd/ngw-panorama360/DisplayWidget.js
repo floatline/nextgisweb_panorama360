e1ba29c7d5c6fd4715ff49211090a2593dea670b
Artemiy <rendrom@gmail.com>
2023-07-06 00:59:04 +0200

    Passing the webmapId prop for DisplayWidget

--- nextgisweb_panorama360/amd/ngw-panorama360/DisplayWidget.js
+++ nextgisweb_panorama360/amd/ngw-panorama360/DisplayWidget.js
@@ -8,9 +8,10 @@ define([
     "@nextgisweb/pyramid/i18n!",
     "@nextgisweb/pyramid/api",
     "ngw-feature-layer/DisplayWidget",
+    "@nextgisweb/pyramid/i18n!",
     "@nextgisweb/gui/react-app",
     "@nextgisweb/panorama360/panorama360-display",
-
+    "@nextgisweb/pyramid/settings!webmap"
 ], function (
     declare,
     i18n,
@@ -21,27 +22,26 @@ define([
     route,
     request,
     DisplayWidget,
+    i18n,
     reactApp,
     Panorama360Display,
-
+    webmapSettings
 ) {
     return declare([DisplayWidget], {
 
-        // var data = plugin.display.get("itemConfig").plugin[plugin.identity];
-        // webmap information
+        title: i18n.gettext("Panorama360"),
 
+        buildRendering: function(){
+			this.inherited(arguments);
+            // To see global webmap settings
+            // TODO: try to update global webmap settings model with panorama default params
+            // console.log(webmapSettings)
 
-        title: "Panorama360",
-        // title: i18n.gettext("Panorama360"),
-        // Uncaught TypeError: i18n.gettext is not a function
-
-        startup: function () {
-            this.inherited(arguments);
-        },
+            console.log(this.webmapId)
+		},
 
         renderValue: function (url) {
-            // var plugin = this.plugin;
-            console.log(this)
+
             if (!this.isValidUrl(url)) {
                 return false;
             };
--- nextgisweb_panorama360/extension.py
+++ nextgisweb_panorama360/extension.py
@@ -13,42 +13,11 @@ class Panorama360Extension(FeatureExtension):
     identity = "panorama360"
 
     display_widget = "ngw-panorama360/DisplayWidget"
-    # "@nextgisweb/panorama360/somecomponent"
-
-
-
-    # def serialize(self, feature):
-
-    #     # FIXME Somehow provide resource id of the webmap
-    #     # Sending the table in a dict to front is a bad idea
-
-    #     db_request = DBSession.query(Panorama360Webmap).all()
-    #     result_dict = {}
-    #     for row in db_request:
-    #         result_dict[str(row.resource_id)] = row.to_dict()
-    #     return (dict(feature.fields), result_dict)
-
 
 
     def serialize(self, feature):
-        enabled =  DBSession.query(Panorama360Webmap).first().enabled
-        panorama_layer_field = DBSession.query(Panorama360Webmap).first().panorama_layer_field
-        if enabled:
-            if panorama_layer_field in feature.fields.keys():
-                url = feature.fields[panorama_layer_field]
-                return url
-        else:
-            return None
+        return feature.fields
 
         
-        
-
     def deserialize(self, feature, data):
-        # obj = Panorama360Webmap.first()
-
-        # if obj is None:
-        #     if data is not None:
-        #         obj = Panorama360Webmap(
-        #             resource_id = self.layer.id
-        #         )
         pass
